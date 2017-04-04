4e985174bf6146e8087b64f9e912d77120b3ab14
hasanello <gasanello@gmail.com>
2017-04-04 22:11:20 +0200

    services page. Web development

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top - 80
+        scrollTop: $(hash).offset().top - 180
       }, 800, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
@@ -33,27 +33,7 @@ $(document).ready(function(){
 
 
 
-$(document).scroll(function(e){
-    var scrollTop = $(document).scrollTop();
-    var screenHeight = $( window ).height();
 
-    if(scrollTop > screenHeight){
-
-        $(".arrow").attr("style","display: none");
-
-        $(".logo").attr("src","images/commentLogo.png");
-
-        $('.navbar').addClass('blur');
-
-        $(".nav-link").attr("id", "blackLink");
-
-    } else {
-        $(".arrow").attr("style","display: block");
-        $(".logo").attr("src","images/whiteLogo.png");
-        $('.navbar').removeClass('blur');
-        $(".nav-link").attr("id","whiteLink");
-    }
-});
 
 
 
