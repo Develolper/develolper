b96637e6b0d6b6b1f3d1eeefaddcc810a308f0c5
hasanello <gasanello@gmail.com>
2017-04-11 16:38:24 +0200

    test path

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+echo $_SERVER['DOCUMENT_ROOT'] . '/swiftmailer/lib/swift_required.php';
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/swiftmailer/lib/swift_required.php';
 if (isset($_POST)) {
     $name = htmlspecialchars($_POST['name']);
@@ -18,7 +21,7 @@ if (isset($_POST)) {
       ->setTo(array('sabir.gojayev007@gmail.com', 'gasanello@gmail.com'))
       ->setBody('New order by: '.$name.'<br /> Contact e-mail: '.$mail.'<br /> Message: '.$message);
     $result = $mailer->send($message);
-}
+
 ?>
 
 
