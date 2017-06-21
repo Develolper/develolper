343101b4f2e41c4480ec137bd4123a53a63ee850
Rahimbeyli Sattar <sattari4@hotmail.com>
2017-06-21 19:58:41 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -18,7 +18,7 @@
                 <ul class="social-icons">
                     <li><a href="https://www.facebook.com/develolper" target="_blank" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
                     <li><a href="https://www.github.com/develolper" target="_blank" class="social-icon"> <i class="fa fa-github"></i></a></li>
-                    <li><a href="https://www.linkedin.com/develolper" target="_blank" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company-beta/18005883/" target="_blank" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
                     <li><a href="https://plus.google.com/develolper" target="_blank" class="social-icon"> <i class="fa fa-google-plus"></i></a></li>
                     <!-- <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li> -->
                     <!-- <li><a href="" class="social-icon"> <i class="fa fa-rss"></i></a></li> -->
